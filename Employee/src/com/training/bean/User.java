3c364b5f3b4ea4f517f2d4cd0aca98962f82b431
shubham.jadhav <Shubham.Jadhav@gmail.com>
2021-10-19 11:39:51 +0530

    create user getter and setter

---
+++ Employee/src/com/training/bean/User.java
@@ -0,0 +1,33 @@
+package com.training.bean;
+
+public class User {
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "User [userid=" + userid + ", userName=" + userName + ", userEmail=" + userEmail + "]";
+	}
+	private int userid;
+	private String userName;
+	private String userEmail;
+}
